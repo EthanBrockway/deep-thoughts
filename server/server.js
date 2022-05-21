4464f67733e4c98253a6e45f82ecc7032a398db9
Skronkie <ebrockway01@gmail.com>
2022-05-20 22:00:50 -0500

    mutations

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 //import typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 const app = express();
